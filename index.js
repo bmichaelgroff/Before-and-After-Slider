6484f8c1b6bfcfbb18c1f34c4a6ecdaf6ad121e3
Michael Groff <bgmichael1400@gmail.com>
2023-04-22 04:58:34 +0000

    Adding range slider

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+document.querySelector('[fitr-slider]').innerHTML += '<input data-slider="1" type="range" min="0" max="100" value="50" aria-label="Percentage of before photo shown" class="slider"/>';
+
 const containers = document.querySelectorAll('[fitr-slider]');
 
 containers.forEach(function (container) {
